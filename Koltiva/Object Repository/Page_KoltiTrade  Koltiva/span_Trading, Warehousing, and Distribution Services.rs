e18d48a330821e9fd8c3612df3463620b8f04067
almasbani <64305692+almasbani@users.noreply.github.com>
2022-09-15 13:43:54 +0700

    Add files via upload

---
+++ Koltiva/Object Repository/Page_KoltiTrade  Koltiva/span_Trading, Warehousing, and Distribution Services.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Trading, Warehousing, and Distribution Services</name>
+   <tag></tag>
+   <elementGuidId>e4397d5b-8c96-45b1-b9ef-8089d197a6cb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>span > span</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='comp-l1prxbmt']/h1/span/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>3e70a021-587c-4c97-9835-0cd4164730c4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Trading, Warehousing, and Distribution Services</value>
+      <webElementGuid>150e48dd-4c2e-49c8-9e3a-192d5a3c8519</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;comp-l1prxbmt&quot;)/h1[@class=&quot;font_0&quot;]/span[1]/span[1]</value>
+      <webElementGuid>c25bfdfc-a61c-485e-a147-10a58d07a56a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='comp-l1prxbmt']/h1/span/span</value>
+      <webElementGuid>5f30b941-6eca-49d9-8cf7-6d0aae80c553</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='KoltiSkills'])[1]/following::span[2]</value>
+      <webElementGuid>c9742ec9-3b23-4af6-81a8-1e471c02ab98</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='KoltiPay'])[1]/following::span[2]</value>
+      <webElementGuid>7fa69559-cd9c-48aa-b8f2-b4849fcd554e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Talk to the Expert'])[1]/preceding::span[1]</value>
+      <webElementGuid>c4c507f4-9db8-49f8-9cf7-e20739e08920</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='KoltiTrade Agri-Input'])[1]/preceding::span[3]</value>
+      <webElementGuid>acb356fa-051a-40be-939e-77ce25fc6712</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Trading, Warehousing, and Distribution Services']/parent::*</value>
+      <webElementGuid>58d8cb3e-5ab0-49a8-9a92-5868ed2a4b30</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//span/span</value>
+      <webElementGuid>2986b376-37a9-4acf-ac87-8e8763443b0d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Trading, Warehousing, and Distribution Services' or . = 'Trading, Warehousing, and Distribution Services')]</value>
+      <webElementGuid>62350ee3-d8a2-4994-b9a0-34009a483af9</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
